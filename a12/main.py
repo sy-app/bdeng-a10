83069a1a0a1d84d0ba2962f7d89065796d01e0fa
sy-app <seijuslp4@icloud.com>
2021-07-12 15:08:50 +0900

    不要な代入を削除

--- a12/main.py
+++ a12/main.py
@@ -6,7 +6,7 @@ from assign_score import *
 def main():
     points, epsilon, min_pts = setting()
     points, clusters = dbscan(points, epsilon, min_pts)
-    points = assign_score(points, clusters)
+    assign_score(points, clusters)
 
 
 if __name__ == '__main__':
